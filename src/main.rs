4d97b9d30c24059053de1e82aaa868ee2e02a1dd
Huck Boles <huck@huck.website>
2023-05-11 14:27:00 -0500

    clean flag

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use color_eyre::Result;
-use metaforge::{build_site, parse_opts};
+use metaforge::{build_site, log, parse_opts};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
     let opts = parse_opts()?;
+
+    log!(
+        opts,
+        format!("cleaning build directory: {}", opts.build.display()),
+        1
+    );
+    if opts.clean && opts.build.exists() {
+        std::fs::remove_dir_all(&opts.build)?;
+    }
+
+    if !opts.build.exists() {
+        std::fs::create_dir(&opts.build)?;
+    }
 
     build_site(&opts)
 }
