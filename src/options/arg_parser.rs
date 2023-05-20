a230664e1ae0554f6329c8619ea14f916fa7cfa9
Huck Boles <huck@huck.website>
2023-05-20 16:36:11 -0500

    edited: arg menu doc comment formatting

--- src/options/arg_parser.rs
+++ src/options/arg_parser.rs
@@ -6,10 +6,10 @@ use clap::Parser;
 #[command(about = "A customizable template driven static site generator")]
 #[command(long_about = None)]
 pub struct Opts {
-    /// root directory [CURRENT_DIR]
+    /// root directory [current_dir]
     #[arg(short, long, value_name = "ROOT_DIR")]
     pub root: Option<String>,
-    /// source file directory [CURRENT_DIR/source]
+    /// source file directory [current_dir/source]
     #[arg(short, long, value_name = "SOURCE_DIR")]
     pub source: Option<String>,
     /// build directory [current_dir/build]
@@ -24,19 +24,19 @@ pub struct Opts {
     /// create a new skeleton directory
     #[arg(long, default_value_t = false)]
     pub new: bool,
-    /// enable extra output. Repeated flags give more info
+    /// enable extra output. repeated flags give more info
     #[arg(short, long, action = clap::ArgAction::Count)]
     pub verbose: u8,
     /// minimal output
     #[arg(short, long, default_value_t = false)]
     pub quiet: bool,
-    /// don't stop on file failure [FALSE]
+    /// don't stop on file failure [false]
     #[arg(long, default_value_t = false)]
     pub force: bool,
-    /// stop on undefined variables and arrays [FALSE]
+    /// stop on undefined variables and arrays [false]
     #[arg(long, default_value_t = false)]
     pub undefined: bool,
-    /// clean build directory before building site [FALSE]
+    /// clean build directory before building site [false]
     #[arg(long, default_value_t = false)]
     pub clean: bool,
     /// don't call pandoc on source files
