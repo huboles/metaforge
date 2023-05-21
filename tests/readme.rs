7398ad18ad3c50e95a7f4bd5fe696f6fbe40e694
Huck Boles <huck@huck.website>
2023-05-21 18:18:10 -0500

    added: working README site

---
+++ tests/readme.rs
@@ -0,0 +1,18 @@
+use eyre::Result;
+
+#[test]
+#[ignore = "generates README site"]
+fn readme() -> Result<()> {
+    let dir = std::path::PathBuf::from("files/README")
+        .canonicalize()
+        .unwrap();
+
+    let mut opts = metaforge::Options::new();
+    opts.root = dir.clone();
+    opts.source = dir.join("source");
+    opts.build = dir.join("build");
+    opts.pattern = dir.join("pattern");
+    opts.clean = true;
+
+    metaforge::build_site(&opts)
+}
